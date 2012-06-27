3e376097c3ce3b785f83ccaa12f2971df5418c22
stevenbandola <bandola.steven@gmail.com>
2012-06-27 14:52:02 -0600

    setting up structure

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="title" content="Proscout">
     <meta name="description" content="The best way to be found">
-    <meta name="keywords" content="Modeling, Fashion, proscout">
-    
-    <link src=''
-    
+    <meta name="keywords" content="Modeling, Fashion, proscout">  
     
     <title>Proscout</title>
 </head>
@@ -38,10 +35,12 @@
         <form action='' method="post">
         	
             
+        	<p>First Name: <input type="text" name="login_fname" id='login_fname' /></p>
+        	<p>Last Name: <input type="text" name="login_lname" id='login_lname' /></p>
         	<p>Email Address: <input type="text" name="login_email" id='login_email' /></p>
             <p>Password: <input type="password" name="login_password" id='login_password' /></p>
-            <p>Remember Me: <input type='checkbox' name='login_remember' id='login_remember'  /></p>
-        	<p><input type='submit' name='login_submit' id='login_submit' value='Login'  /></p>
+            <p>Confirm Password: <input type="password" name="login_cpassword" id='login_cpassword' /></p>
+        	<p><input type='submit' name='signup_submit' id='signup_submit' value='Signup'  /></p>
         	
         </form>
         
