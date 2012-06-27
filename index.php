0ccad07a2b3718d7a169c7d18cff4a1f847ae883
stevenbandola <bandola.steven@gmail.com>
2012-06-27 14:42:46 -0600

    laying out some modals

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta name="description" content="The best way to be found">
     <meta name="keywords" content="Modeling, Fashion, proscout">
     
+    <link src=''
+    
+    
     <title>Proscout</title>
 </head>
 
@@ -16,6 +19,33 @@
     <a href="#">Get started</a>
     <a href="#">Log In</a>
     <a href="#">Signup</a>
+    
+    <div class='jqModal' id='modal_login'>
+		
+        <form action='' method="post">
+        
+        	<p>Email Address: <input type="text" name="login_email" id='login_email' /></p>
+            <p>Password: <input type="password" name="login_password" id='login_password' /></p>
+            <p>Remember Me: <input type='checkbox' name='login_remember' id='login_remember'  /></p>
+        	<p><input type='submit' name='login_submit' id='login_submit' value='Login'  /></p>
+        	
+        </form>
+        
+    </div>
+    
+    <div class='jqModal' id='modal_signup'>
+		
+        <form action='' method="post">
+        	
+            
+        	<p>Email Address: <input type="text" name="login_email" id='login_email' /></p>
+            <p>Password: <input type="password" name="login_password" id='login_password' /></p>
+            <p>Remember Me: <input type='checkbox' name='login_remember' id='login_remember'  /></p>
+        	<p><input type='submit' name='login_submit' id='login_submit' value='Login'  /></p>
+        	
+        </form>
+        
+    </div>  
 
 </body>
 </html>
